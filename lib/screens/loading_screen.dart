b2b1c6acb2464e3ade723421c8c0944d76d09ce5
Matheus Schneider <matheus.ssm@gmail.com>
2020-09-22 11:58:24 -0300

    updated: initial tests with jsonDecode lib

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:http/http.dart' as http;
@@ -23,7 +25,12 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   void getData() async {
     http.Response response = await http.get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
-    print(response.body);
+    String data = response.body;
+
+    var longitude = jsonDecode(data)['coord']['lon'];
+    var weatherDescription = jsonDecode(data)['weather'][0]['description'];
+    print(longitude);
+    print(weatherDescription);
   }
 
   @override
