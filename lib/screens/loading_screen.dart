4a55eee8bd65397edc00ef2ff270e2558063124a
Matheus Schneider <matheus.ssm@gmail.com>
2020-09-23 11:43:57 -0300

    updated: extraced location methods to a service

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,8 +1,6 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
-import 'package:http/http.dart' as http;
+import 'package:clima/services/networking.dart';
 
 const apiKey = 'bb6b801733ae870e8ac41adc19b94803';
 
@@ -18,28 +16,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
 
     latitude = location.latitude;
     longitude = location.longitude;
 
-    getData();
-  }
-
-  void getData() async {
-    http.Response response = await http.get(
+    NetworkHelper networkHelper = NetworkHelper(
         'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
-    String data = response.body;
 
-    var longitude = jsonDecode(data)['coord']['lon'];
-    var weatherDescription = jsonDecode(data)['weather'][0]['description'];
-    print(longitude);
-    print(weatherDescription);
+    var weatherData = await networkHelper.getData();
   }
 
   @override
